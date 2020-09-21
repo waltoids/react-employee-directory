611bc1f4f2fa7ba8471cc0af792cdbfcbd4538d4
Walter Joy IV <waltoids@users.noreply.github.com>
2020-09-20 21:24:14 -0400

    fixed table issue with using wrong compnent

--- src/App.js
+++ src/App.js
@@ -1,32 +1,70 @@
 import React, { useState, useEffect } from 'react';
-import DataTable from './components/TableData'
-import SearchObject from './utils/SearchObject'
-import DataContext from './utils/DataContext';
-import employees from './utils/employees.json'
+import TableData from './components/TableData'
+import SearchObject from './utils/SearchObject';
+import {data as employees} from './utils/employees.json';
+import Main from './components/Main';
+import Jumbotron from './components/Jumbotron'
+import SearchBar from './components/SearchBar'
 
 export default function App() {
   const [data, setData] = useState([])
 
-   const [query, setQuery] = useState('')
-   const onChangeSearch = (e) => {
-     setQuery(event.target.value)
-   }
+  const [query, setQuery] = useState('')
+
+  const onChangeSearch = (e) => {
+    setQuery(e.target.value)
+  }
 
   useEffect(() => {
     setData(SearchObject(employees, query))
   },[query]);
 
-search
-onclick(button) sort the table by name ascending or decending 
+  const [sortConfig, setSortConfig] = React.useState({ direction: 'descending', key: 'id' });
+
+  const sortedItems = React.useMemo(() => {
+    const sortableItems = [...data];
+    if (sortConfig !== null) {
+      sortableItems.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return sortableItems;
+  }, [data, sortConfig]);
+
+  const requestSort = (key) => {
+    let direction = 'ascending';
+    if (sortConfig && sortConfig.key === key && sortConfig.direction === 'ascending') {
+      direction = 'descending';
+    }
+    setSortConfig({ key, direction });
+    setData(sortedItems);
+  };
+
+  const getDirection = (name) => {
+    if (sortConfig) {
+      return sortConfig.key === name ? sortConfig.direction : undefined;
+    }
+    return null;
+  };
 
   return(
-    <DataContext.Provider
-      value={{data}}>
-      <div>
-        <div>Filter</div>
-        <div> <DataTable data={data.users}/> </div>
-      </div>
-    </DataContext.Provider>
+    <div className="App">
+    <Jumbotron title="Employee Directory" />
+      
+      <Main>
+        <div className="mb-4 d-flex justify-content-around">
+          <SearchBar onChange={onChangeSearch} />
+        </div>
+        <TableData getDirection={getDirection} requestSort={requestSort} data={data} />
+      </Main>
+      
+  </div>
   );
 }
 
